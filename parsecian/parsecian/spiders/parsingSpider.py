2bdeb4230ad3b4b967683b762a0f4104bd4602fc
aidarmar123 <145375244+aidarmar123@users.noreply.github.com>
2024-05-19 01:49:47 +0500

    Add count page and edit price apartment

--- parsecian/parsecian/spiders/parsingSpider.py
+++ parsecian/parsecian/spiders/parsingSpider.py
@@ -10,9 +10,10 @@ class ParsingspiderSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(restrict_xpaths='//a[@class="_93444fe79c--media--9P6wN"]'), callback='parse_item',follow=True),
-        Rule(LinkExtractor(restrict_xpaths='//ul[@class="_93444fe79c--pages-list--gsEUE"]/../a'))
+        Rule(LinkExtractor(restrict_xpaths='//ul[@class="_93444fe79c--pages-list--gsEUE"]/../a'),  callback='parse_links', follow=True)
             )
-
+    
+    link_counter=1
     def parse_item(self, response):
         item = {}
         
@@ -21,7 +22,10 @@ class ParsingspiderSpider(CrawlSpider):
         item["square"]=response.xpath('//span[text()="Общая площадь"]/../span[contains(text(),"м²")]/text()').get()
         item["floor"]=response.xpath('//span[text()="Этаж"]/../span[contains(text(),"из")]/text()').get()
         for part_adress in response.xpath('//div[@data-name="AddressContainer"]'):
-            item["stret"]=part_adress.xpath('.//a/text()').getall()
-        item["price"]=response.xpath('//span[contains(text(),"₽") ]/text()').get()
+            item["street"]=part_adress.xpath('.//a/text()').getall()
+        item["price"]=response.xpath('//span[contains(text(),"₽") and @class="a10a3f92e9--color_black_100--Ephi7 a10a3f92e9--lineHeight_9u--limEs a10a3f92e9--fontWeight_bold--BbhnX a10a3f92e9--fontSize_28px--P1gR4 a10a3f92e9--display_block--KYb25 a10a3f92e9--text--e4SBY" ]/text()').get()
         item["id"] = response.url.split('/')[-2]
+        item["page_number"]=self.link_counter
         return item
+    def parse_links(self, response):
+        self.link_counter += 1
